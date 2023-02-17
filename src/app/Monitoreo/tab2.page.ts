097c45de3e118f5c645dd022fd9c4b534165ab4a
ErnestoJESB <108556821+ErnestoJESB@users.noreply.github.com>
2023-02-17 09:27:21 -0500

    cambio monitoreo2/neto

--- src/app/Monitoreo/tab2.page.ts
+++ src/app/Monitoreo/tab2.page.ts
@@ -10,7 +10,38 @@ import { AlertController } from '@ionic/angular';
 })
 export class Tab2Page {
 
+  isToggleChecked1:boolean=false;
+  isToggleChecked2:boolean=false;
+  isToggleChecked3:boolean=false;
   constructor(private alertController: AlertController) {}
+
+  onToggleChange1(){
+    if(!this.isToggleChecked1){
+      this.presentAlert();
+      this.isToggleChecked1=true;
+    }
+    else{
+      this.isToggleChecked1=false;
+    }
+  }
+  onToggleChange2(){
+    if(!this.isToggleChecked2){
+      this.presentAlert();
+      this.isToggleChecked2=true;
+    }
+    else{
+      this.isToggleChecked2=false;
+    }
+  }
+  onToggleChange3(){
+    if(!this.isToggleChecked3){
+      this.presentAlert();
+      this.isToggleChecked3=true;
+    }
+    else{
+      this.isToggleChecked3=false;
+    }
+  }
 
   async presentAlert() {
     const alert = await this.alertController.create({
