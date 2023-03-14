23d303000cb7f72624bd2aa6c72d5c2477aa6b5d
CahuichG17 <78057978+CahuichG17@users.noreply.github.com>
2023-03-13 21:51:47 -0500

    IOT#17-Scatter-Chart-Added
    gta 5

--- src/app/graficas/graficas.page.ts
+++ src/app/graficas/graficas.page.ts
@@ -14,61 +14,100 @@ export class GraficasPage implements OnInit, AfterViewInit {
   // that we have added to the canvas element in the HTML template.
   @ViewChild('doughnutCanvas') private doughnutCanvas!: ElementRef;
   @ViewChild('lineCanvas') private lineCanvas!: ElementRef;
-  doughnutChart: any;
+  barChart: any;
   lineChart: any;
   data: any;
   private chart!: Chart;
+  scatterChart: any;
+  historialH: any;
 
-  constructor(private dataService: RealtimeDatabaseService) {
+
+  constructor(
+    private dataService: RealtimeDatabaseService) {
   
   }
 
   ngOnInit() {
     this.dataService.getData().subscribe(data => {
       this.data = data;
-      this.doughnutChartMethod();
       this.lineChartMethod();
+      this.scatterChartMethod();
     });
   }
   
   ngAfterViewInit() {
     this.dataService.getData().subscribe(data => {
       this.data = data;
-      this.doughnutChartMethod();
       this.lineChartMethod();
+      this.scatterChartMethod();
     });
   }
-  doughnutChartMethod() {
-    if (this.doughnutChart) {
+  scatterChartMethod() {
+    if (this.scatterChart) {
       console.log('algo-|', this.chart)
-      this.doughnutChart.destroy(); // Elimina la instancia anterior
+      this.scatterChart.destroy(); // Elimina la instancia anterior
     }
-    let datos = this.data.historial_temperatura
-    this.doughnutChart = new Chart(this.doughnutCanvas.nativeElement, {
-      type: 'doughnut',
-      data: {
-        labels: Object.keys(datos.historico),
-        datasets: [{
-          label: datos.tittle,
-          data: Object.values(datos.historico),
-          backgroundColor: [
-            'rgba(255, 159, 64, 0.2)',
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)'
-          ],
-          hoverBackgroundColor: [
-            '#FFCE56',
-            '#FF6384',
-            '#36A2EB',
-            '#FFCE56',
-            '#FF6384'
-          ]
-        }]
-      }
-    });
+
+    
+let datos = this.data.historial_temperatura;
+
+this.scatterChart = new Chart(this.doughnutCanvas.nativeElement, {
+  type: 'scatter',
+  data: {
+    labels: Object.keys(datos.historialH),
+    datasets: [{
+      label: datos.tittle,
+      data: Object.values(datos.historialH).map((y, i) => ({x: i, y})),
+      backgroundColor: 'rgba(255, 159, 64, 0.2)',
+      borderColor: 'rgba(255, 159, 64, 1)',
+      borderWidth: 1,
+      pointBackgroundColor: 'rgba(255, 159, 64, 1)',
+      pointBorderColor: '#fff',
+      pointBorderWidth: 1,
+      pointRadius: 5,
+      pointHoverRadius: 8
+    }],
+
+
+    
+  },
+
+  options: {}
+  
+});
+
+  //   let datos = this.data.historial_temperatura
+  //   this.scatterChart = new Chart(this.scatterCanvas.nativeElement, {
+  //     type: 'scatter',
+  //     data: {
+  //       labels: Object.keys(datos.historialH),
+  //       datasets: [{
+  //         label: datos.tittle,
+  //         data: Object.values(datos.historialH),
+  //         backgroundColor: [
+  //           'rgba(255, 159, 64, 0.2)',
+  //           'rgba(54, 162, 235, 0.2)',
+  //           'rgba(255, 206, 86, 0.2)',
+  //           'rgba(75, 192, 192, 0.2)',
+  //           'rgba(153, 102, 255, 0.2)',
+  //           'rgba(255, 99, 132, 0.2)',
+  //           'rgba(255, 159, 64, 0.2)',
+  //         ],
+  //         hoverBackgroundColor: [
+  //           '#FFCE56',
+  //           '#FF6384',
+  //           '#36A2EB',
+  //           '#FFCE56',
+  //           '#FF6384',
+  //           '#FF6384',
+  //           '#FF6384',
+  //         ]
+  //       }]
+  //     }
+  //   });
+  // }
   }
+  
   lineChartMethod() {
     if (this.lineChart) {
       this.lineChart.destroy();
@@ -77,7 +116,7 @@ export class GraficasPage implements OnInit, AfterViewInit {
     this.lineChart = new Chart(this.lineCanvas.nativeElement, {
       type: 'line',
       data: {
-        labels: Object.keys(datos.historico),
+        labels: Object.keys(datos.historial),
         datasets: [
           {
             label: datos.tittle,
@@ -97,7 +136,7 @@ export class GraficasPage implements OnInit, AfterViewInit {
             pointHoverBorderWidth: 2,
             pointRadius: 1,
             pointHitRadius: 10,
-            data: Object.values(datos.historico),
+            data: Object.values(datos.historial),
             spanGaps: false,
           }
         ]
@@ -105,5 +144,4 @@ export class GraficasPage implements OnInit, AfterViewInit {
     });
   }
 
-  
 } // Elimina la instancia anterior
