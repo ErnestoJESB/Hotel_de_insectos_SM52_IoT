0cba52c024bc4dffbfd73c48aa78d59a807d8311
ErnestoJESB <108556821+ErnestoJESB@users.noreply.github.com>
2023-03-16 15:05:24 -0500

    Se inicia la lógica para promediar temperatura

--- src/app/Monitoreo/tab2.page.ts
+++ src/app/Monitoreo/tab2.page.ts
@@ -35,21 +35,78 @@ export class Tab2Page {
   data: any;
   data1: any;
   data2: any;
+  
 
   ngOnInit() {
-    this.dataService.getData().subscribe(data => {
-      this.data = data;
-      console.log(this.data)
-    });
-    this.dataService.leerDatos('/seguridad/estado').subscribe((data1) => {
-      this.data1 = data1;
-      this.toggleValue = this.data1; // Actualiza el valor del toggle
-  });
-this.dataService.leertemp('/seguridad/temperatura').subscribe((data2) => {
-  this.data2 = data2;
-  console.log(this.data2);
-  });
-  }
+        let templocal: number;
+        
+        this.dataService.getData().subscribe(data => {
+          this.data = data;
+          console.log(this.data)
+        });
+        this.dataService.leerDatos('/seguridad/estado').subscribe((data1) => {
+          this.data1 = data1;
+          this.toggleValue = this.data1; // Actualiza el valor del toggle
+      });
+        this.dataService.leertemp('/seguridad/temperatura').subscribe((data2) => {
+          this.data2 = data2;
+          console.log(this.data2);
+          templocal = this.data2;
+        console.log(templocal);
+          });
+
+        //A partir de aquí se obtiene la fecha y se compara con el día de la semana
+        let fecha: Date = new Date();
+        let diaSemana: number = fecha.getDay(); // obtiene el día de la semana en formato numérico (0-6)
+        console.log(diaSemana);
+
+        const diaSem: string[] = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
+
+        console.log(`${diaSem[diaSemana]}`);
+        let ruta: string = '/seguridad/dia';
+        let datos: any;
+        let totaltemp: number = 0;
+        let tempmedia: number = 0;
+        let contador: number = 0;
+      switch (diaSemana) {
+        case 0:
+        datos = "Domingo";
+        this.dataService.dia(ruta,datos);
+          break;
+        case 1:
+          datos = "Lunes";
+          this.dataService.dia(ruta,datos);
+          this.dataService.leertemp('/seguridad/temperatura').subscribe((data2) => {
+            this.data2 = data2;
+            templocal = this.data2;
+            totaltemp = totaltemp + templocal;
+            contador = contador++;
+            tempmedia = totaltemp / contador;
+
+            });
+          break;
+        case 2:
+          datos = "Martes";
+          this.dataService.dia(ruta,datos);
+          break;
+        case 3:
+          datos = "Miércoles";
+          this.dataService.dia(ruta,datos);
+          break;
+        case 4:
+          datos = "Jueves";
+          this.dataService.dia(ruta,datos);
+          break;
+        case 5:
+          datos = "Sábado";
+          this.dataService.dia(ruta,datos);
+          break;
+        default:
+          console.log("Día no válido");
+      }
+    }
+
+  
 
     enviardatos() {
       if (this.data1 == false) {
@@ -92,8 +149,16 @@ this.dataService.leertemp('/seguridad/temperatura').subscribe((data2) => {
 
   /* Crea un constructor */
   temperatura() { 
-    let templocal = this.data2;
+    this.dataService.leertemp('/seguridad/temperatura').subscribe((data2) => {
+      this.data2 = data2;
+      console.log(this.data2);
+      });
+
+    let templocal: number = this.data2;
     console.log(templocal);
+
+    let date: Date = new Date();
+    console.log("Date = " + date);
   }
   
 }
